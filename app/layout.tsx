5aa4b1ba7b766afdc046b6c538836f69db28d803
DvaDvaD <davidlou0810@gmail.com>
2024-06-05 17:00:44 +0700

    :sparkles: Add progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,37 @@
-import { Urbanist } from 'next/font/google'
+import { Urbanist } from "next/font/google";
 
-import ModalProvider from '@/providers/modal-provider'
-import ToastProvider from '@/providers/toast-provider'
-import Navbar from '@/components/navbar'
-import Footer from '@/components/footer'
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
-import './globals.css'
+import "./globals.css";
+import ProgressBarProvider from "@/components/progress-bar";
 
-const font = Urbanist({ subsets: ['latin'] })
+const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Store',
-  description: 'Store - The place for all your purchases.',
-}
+  title: "Store",
+  description: "Store - The place for all your purchases.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
         <ToastProvider />
         <ModalProvider />
-        <Navbar />
-        {children}
-        <Footer />
+
+        <ProgressBarProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </ProgressBarProvider>
       </body>
     </html>
-  )
+  );
 }
---
+++ components/progress-bar.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { Next13ProgressBar } from "next13-progressbar";
+
+const ProgressBarProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      {children}
+      <Next13ProgressBar
+        height="3px"
+        startPosition={0.4}
+        color="#000000"
+        stopDelayMs={400}
+        options={{ showSpinner: false }}
+        showOnShallow
+      />
+    </>
+  );
+};
+
+export default ProgressBarProvider;
