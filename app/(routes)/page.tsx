63955311edeb29be5be55f22ca3749c4963f0000
DvaDvaD <davidlou0810@gmail.com>
2024-06-05 16:56:11 +0700

    :bug: Fix unusable code

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,20 +8,18 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("9da06b4e-cbed-45d3-948a-28e3b198b50f");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
-          data={billboard}
-        />
+        <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
       </div>
     </Container>
-  )
+  );
 };
 
 export default HomePage;
